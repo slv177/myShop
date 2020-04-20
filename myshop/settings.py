63dd8d453ce9a2a9884d982ad89b8f554ce74afa
Slv177 <shambazov@gmail.com>
2020-04-20 14:27:48 +0200

    RabbitMQ, Celery and flower for serving email notifications.

--- myshop/settings.py
+++ myshop/settings.py
@@ -32,6 +32,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CART_SESSION_ID = 'cart'
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
 
 # Application definition
 
--- orders/views.py
+++ orders/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .forms import OrderCreateForm
 from .models import OrderItem
 from cart.cart import Cart
+from .tasks import order_created
 
 def order_create(request):
     cart = Cart(request)
@@ -16,9 +17,10 @@ def order_create(request):
                                          quantity=item['quantity'])
 
             cart.clear()
-            return render(request,
-                          'orders/order/created.html',
-                          {'order': order})
+            order_created.delay(order.id)
+            # return render(request,
+            #               'orders/order/created.html',
+            #               {'order': order})
     else:
         form = OrderCreateForm()
     return render(request,
